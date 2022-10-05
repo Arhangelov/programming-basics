063a23dd923130bc22c9b90ff662e80eecc99bf7
Petyo Arhangelov <arhangelov93@gmail.com>
2022-10-06 00:01:46 +0300

    first exercises

---
+++ area.js
@@ -0,0 +1,8 @@
+function rectangleArea(){
+    let a = 2;
+    let b = 7;
+
+    let result = a * b;
+    console.log(result);
+}
+rectangleArea();
---
+++ hello.js
@@ -0,0 +1,6 @@
+function Hello(){
+    let name = "SoftUni";
+    console.log("Hello SoftUni");
+    console.log(name);
+}
+Hello();
