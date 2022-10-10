c63993779081b1b3cbc7546c9070a02654286d0d
Petyo Arhangelov <arhangelov93@gmail.com>
2022-10-11 00:15:43 +0300

    day of the week exercise

---
+++ DayOfTheWeek.js
@@ -0,0 +1,22 @@
+function dayOFTheWeek(input){
+    let day = Number(input.shift());
+
+    switch (day) {
+        case 1: console.log("Monday");
+            
+            break;
+
+        case 2: console.log ("Tuesday"); break;
+        case 3: console.log ("Wednesday"); break;
+        case 4: console.log ("Thursday"); break;
+        case 5: console.log ("Friday"); break;
+        case 6: console.log ("Saturday"); break;
+        case 7: console.log ("Sunday"); break;
+    
+        default: console.log ("Error");
+            break;
+    }
+}
+
+dayOFTheWeek ([345
+])
