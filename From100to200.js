1cb201317a3fc5fbb0dcc318de120cfcfc9ccb73
Petyo Arhangelov <arhangelov93@gmail.com>
2022-11-14 23:43:44 +0200

    exercise from 100 to 200

---
+++ From100to200.js
@@ -0,0 +1,15 @@
+function num100To200 (input){
+    let num = Number(input.shift());
+
+    if(num < 100){
+        console.log("Less than 100");
+    }else if (num <=200){
+        console.log("Between 100 and 200");
+    }else{
+        console.log("Greater than 200");
+    }
+}
+
+num100To200([
+    100
+])
