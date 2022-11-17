2044d45d2626777ae4394fdc9711184067905fbf
Petyo Arhangelov <arhangelov93@gmail.com>
2022-11-18 00:41:37 +0200

    fruit or vegetable exercise

---
+++ FruitOrVegetable.js
@@ -0,0 +1,24 @@
+function fruitOrVegetable (input){
+    let fOrV = input.shift();
+
+    switch (fOrV){
+        case "banana":
+        case "apple":
+        case "kiwi":
+        case "cherry":
+        case "lemon":
+        case "grapes": console.log ("fruit");
+        break;
+        case "tomato":
+        case "cucumber":
+        case "pepper":
+        case "carrot": console.log ("vegetable");
+        break;
+        default: console.log ("unknown");
+        
+    }
+}
+
+fruitOrVegetable([
+    "bananan"
+])
