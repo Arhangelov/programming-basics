d55536cdb9d029851939440ef1fa246918ef6b5f
Petyo Arhangelov <arhangelov93@gmail.com>
2023-01-10 23:39:34 +0200

    invalid number exercise

---
+++ InvalidNumber.js
@@ -0,0 +1,17 @@
+function invalidNumber (input){
+    let n = Number(input.shift());
+
+    if (n === 0){
+
+    }
+    else if ((n < 100)||(n > 200) ){
+        console.log ("invalid");
+    }
+    else if ((n >= 100) || (n <= 200)){
+        
+    }
+}
+
+invalidNumber ([
+    0
+])
