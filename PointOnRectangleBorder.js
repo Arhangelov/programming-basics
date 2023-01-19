4877906649843cc00735e7b61897fe2c9c931c4d
Petyo Arhangelov <arhangelov93@gmail.com>
2023-01-19 15:49:59 +0200

    point on rectangle border exercise

---
+++ PointOnRectangleBorder.js
@@ -0,0 +1,17 @@
+function pointOnRectangleBorder (input){
+    let x1 = Number (input.shift());
+    let y1 = Number (input.shift());
+    let x2 = Number (input.shift());
+    let y2 = Number (input.shift());
+    let x = Number (input.shift());
+    let y = Number (input.shift());
+
+    let firstCondition = ((x==x1 || x==x2) && (y >= y1 && y <=y2));
+    let secondCondition = ((y==y1 || y==y2) && (x >=x1 && x <= x2));
+
+    if (firstCondition || secondCondition){
+        console.log ("Border");
+    }else{
+        console.log ("Inside / Outside")
+    }
+}
